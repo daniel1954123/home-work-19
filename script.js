391083e1ca74212521774688b3303b8b52468c88
“daniel1954123” <m.danik20.07@gmail.com>
2021-08-21 23:00:19 +0600

    home-work19

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// const Fruits = {
+//     name:null,
+//     price:'',
+//     amount:'',
+// }
+//
+// const fruit = Object.create(Fruits)
+//
+// fruit.name = 'Apple'
+// fruit.price = 4
+// fruit.amount = '49c'
+//
+// Object.getPrototypeOf(Fruits)
+//
+// console.log(Fruits)
+
+// const user = {
+//     name: "Kate",
+//     age:20,
+//     city: 'London'
+// }
+//
+// const user2 = Object.create(user)
+//
+// user2.age = 'Apple'
+//
+// console.log(Object.create(user2))
+//
+// Object.setPrototypeOf(user,null)
+//
+// console.log(user)
+
+
+
